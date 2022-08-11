9507afb13b4f17cd06bb10131698923a6dc9146b
Artem Ostrovskiy <asostrovskiy@yandex.ru>
2022-08-11 21:23:49 +0400

    shadow

--- src/components/weather-widget.vue
+++ src/components/weather-widget.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-loading="loading">
-    <div v-for="city in weatherList" :key="city?.id" class="widjet">
+  <div
+    v-loading="loading"
+  >
+    <div
+      v-for="city in weatherList"
+      :key="city?.id"
+      class="widjet"
+    >
       <div>{{ city.name }}, {{ city.sys.country }} </div>
       <div>
         <img :src="`http://openweathermap.org/img/w/${city.weather[0].icon}.png`" alt="weather" />
@@ -8,10 +14,12 @@
       </div>
       <div>
         Ущущается как {{ city.main.feels_like }}℃.  <br/>
-        {{ city.weather[0].description }}
+        {{ city.weather[0].description.split('').splice(0,1).join('').toUpperCase() +
+        city.weather[0].description.split('').slice(1).join('') }}.
       </div>
-      <div></div>
-      <div></div>
+      <div>Рассвет: {{ getTime(city.sys.sunrise) }}</div>
+      <div>Закат: {{ getTime(city.sys.sunset) }}</div>
+      <div>Ветер: {{ Math.round(city.wind.speed) }}м/с</div>
 
     </div>
   </div>
@@ -56,12 +64,21 @@
           })
           .finally(():void => { this.loading = false })
       },
-    },
+      getTime ( time:number ):string {
+        const date =new Date( time*1000 )
+        const hour = date.getHours()
+        const minutes = date.getMinutes()
+        return `${hour > 10 ? hour : `0${hour}` }:${minutes > 10 ? minutes : `0${minutes}` }`
+      }
+    }
   })
 </script>
 
 <style lang="scss">
   .widjet {
     height: fit-content;
+    box-shadow: 1px 5px 10px;
+    padding: 10px;
+    margin-bottom: 10px;
   }
 </style>
